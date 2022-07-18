d5374b40cb942ca5e4ad8b663c9266cc339a1f1a
jagjot93 <71182607+jagjot93@users.noreply.github.com>
2022-07-19 01:19:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-  version = "3.18.0"
   region = "us-east-1"
   assume_role {
-    role_arn = "arn:aws:iam::459772773625:role/admin-access"
+    role_arn = "arn:aws:iam::930392243596:role/admin-access"
   }
 }
