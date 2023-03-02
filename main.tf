efd6901f3b47c91758d09718ebd194dfcaedce38
jagjot93 <71182607+jagjot93@users.noreply.github.com>
2023-03-02 18:55:33 +0530

    Update IAM role

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"
   assume_role {
-    role_arn = "arn:aws:iam::930392243596:role/admin-access"
+    role_arn = "arn:aws:iam::053969469615:role/tf-admin-access"
   }
 }
